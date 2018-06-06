f3c359b960521cd60a450421714923b5143c428b
jaronschulz <jaronschulz@gmail.com>
2018-06-06 20:34:58 +0200

    finished Part 3

--- learnYouNode.js
+++ learnYouNode.js
@@ -9,3 +9,8 @@
 // console.log(arr);
 
 //* Part 3
+// const fs = require('fs');
+
+// const str = fs.readFileSync(process.argv[2]).toString();
+// const newLines = str.split('\n').length-1;
+// console.log(newLines);
