f34d30d6103870bf6f040d745fd3af3d63ed80a8
jaronschulz <jaronschulz@gmail.com>
2018-06-06 19:57:32 +0200

    finished Part 2

--- learnYouNode.js
+++ learnYouNode.js
@@ -1,1 +1,11 @@
-console.log("HELLO WORLD");
+//* Part 1
+// console.log("HELLO WORLD");
+
+//* Part 2
+// let arr = 0;
+// for (let index = 2; index < process.argv.length; index++) {
+//     arr += Number(process.argv[index]);
+// }
+// console.log(arr);
+
+//* Part 3
